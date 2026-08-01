ebf98b5d204baefccd05297b9a0eb88bff521014
devbidden <obunadikechioma8@gmail.com>
2026-08-01 20:04:36 +0100

    Describe your changes

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 require("./src/app");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Scraper is running");
+});
+
+app.listen(process.env.PORT || 3000);
